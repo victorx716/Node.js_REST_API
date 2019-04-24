f094ee26bda803864f15ec252451046c29b72a65
Victor Liu <victorliu716@gmail.com>
2019-04-23 22:18:19 -0500

    Implement basic APIs.
    Including error handing next for all routes.

---
+++ api/routes/products.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Successfully handling GET requests to /products'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Successfully handling POST requests to /products'
+  })
+})
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  if (id === '88') {
+    res.status(200).json({
+      message: 'This is the lucky ID',
+      id: id
+    });
+  } else {
+    res.status(200).json({
+      message: 'This is a valid ID'
+    });
+  }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Product has been updated'
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Product is removed from your cart!'
+  });
+});
+
+module.exports = router;
